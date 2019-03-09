2f5e37cb85b53203d666b74a41da2603e7572a13
Michał Frontczak <michaljev@o2.pl>
2019-03-09 11:56:27 +0100

    Added Volley sample request (without required permissions)

--- app/src/main/java/pl/krakow/up/ii/opensource/mobilewu/MainActivity.java
+++ app/src/main/java/pl/krakow/up/ii/opensource/mobilewu/MainActivity.java
@@ -4,12 +4,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
 
     // https://developer.android.com/reference/android/widget/Button
-    Button btn;
+    private Button btn;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,20 +26,34 @@ public class MainActivity extends AppCompatActivity {
 
         // https://developer.android.com/reference/android/view/View.html#findViewById(int)
         btn = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
 
         btn.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                // https://developer.android.com/reference/android/widget/Toast
+                // https://developer.android.com/training/volley/simple.html
+                RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+                String url ="https://wu.up.krakow.pl/WU/";
 
-                // Toast z tekstem zdefiniowanym w res/values/strings.xml
-                // CharSequence txt_main_toast = getResources().getText(R.string.txt_main_toast);
-                // Toast.makeText(MainActivity.this, txt_main_toast, Toast.LENGTH_SHORT)
-                //          .show();
+                // Request a string response from the provided URL.
+                StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                // Display the first 500 characters of the response string.
+                                textView.setText("Response is: "+ response.substring(0,500));
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        textView.setText("That didn't work!");
+                    }
+                });
 
-                Toast.makeText(MainActivity.this, "Toast!", Toast.LENGTH_SHORT)
-                        .show();
+                // Add the request to the RequestQueue.
+                queue.add(stringRequest);
+
             }
         });
     }

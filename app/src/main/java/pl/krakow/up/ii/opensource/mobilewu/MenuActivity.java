41fbabe8e1c30ac3348e158b404e1bdc005986e1
Dominik Czechowski <czechow.dominik@gmail.com>
2019-04-01 03:01:23 +0200

    - zwiększenie SDK z 23 do 24 żeby czytało okhttp3 - dodanie dependensów - dodanie MenuActivity z ListView do możliwości dodania innych Activity które robi reszta, - MenuActiviry: ikona menu na górnym pasku do wylogowania się i sprawdzenia danych użytkownika - blokada przejścia się do poprzedniego activity po wylogowaniu - dodanie progressBar przy logowaniu, - dodanie wstępnego activity z ocenami

---
+++ app/src/main/java/pl/krakow/up/ii/opensource/mobilewu/MenuActivity.java
@@ -0,0 +1,96 @@
+package pl.krakow.up.ii.opensource.mobilewu;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MenuActivity extends AppCompatActivity {
+
+    ListView listView;
+    String[] listValue = new String[] {"Oceny","Plan studiów","Plan zajęć","Stypendia"};
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.example_menu, menu);
+        return true;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+        listView = (ListView)findViewById(R.id.listView1);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_2, android.R.id.text1, listValue);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position){
+                    case 0:
+                        Toast.makeText(getApplicationContext(), "Wybrano Oceny", Toast.LENGTH_SHORT).show();
+                        System.out.println("Wybrano 0");
+                        Intent intent = new Intent(MenuActivity.this,OcenyActivity.class);
+                        startActivity(intent);
+                        break;
+                    case 1:
+                        Toast.makeText(getApplicationContext(), "Wybrano Plan studiów", Toast.LENGTH_SHORT).show();
+                        System.out.println("Wybrano 1");
+                        //Intent intent = new Intent(MenuActivity.this, [tu wpisujesz swoje activity.class]);
+                        //startActivity(intent);
+                        break;
+                    case 2:
+                        Toast.makeText(getApplicationContext(), "Wybrano zajęć", Toast.LENGTH_SHORT).show();
+                        System.out.println("Wybrano 2");
+                        //Intent intent = new Intent(MenuActivity.this, [tu wpisujesz swoje activity.class]);
+                        //startActivity(intent);
+                        break;
+                    case 3:
+                        Toast.makeText(getApplicationContext(), "Wybrano Stypendia", Toast.LENGTH_SHORT).show();
+                        System.out.println("Wybrano 3");
+                        //Intent intent = new Intent(MenuActivity.this, [tu wpisujesz swoje activity.class]);
+                        //startActivity(intent);
+                        break;
+                }
+
+            }
+        });
+
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.item1:
+                Toast.makeText(this, "Wybrano Opcje", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.subitem1:
+                Toast.makeText(this, "Wylogowano", Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent(MenuActivity.this, MainActivity.class);
+                finish();
+                startActivity(intent);
+                return true;
+            case R.id.subitem2:
+                Toast.makeText(this, "Wybrano Dane studenta", Toast.LENGTH_SHORT).show();
+                //tutaj dodaj swoje activity tak jak u góry tylko bez finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+
+    }
+}
--- app/src/main/java/pl/krakow/up/ii/opensource/mobilewu/OcenyActivity.java
+++ app/src/main/java/pl/krakow/up/ii/opensource/mobilewu/OcenyActivity.java
@@ -1,15 +1,15 @@
 package pl.krakow.up.ii.opensource.mobilewu;
 
-
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
 import android.content.Intent;
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import org.jsoup.Jsoup;
@@ -18,6 +18,8 @@ import org.jsoup.nodes.Element;
 import org.riversun.okhttp3.OkHttp3CookieHelper;
 
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
@@ -25,13 +27,16 @@ import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 
-public class MainActivity extends AppCompatActivity {
+
+
+import org.riversun.okhttp3.OkHttp3CookieHelper;
+
+public class OcenyActivity extends AppCompatActivity {
 
     final OkHttp3CookieHelper cookieHelper = new OkHttp3CookieHelper();
 
-    EditText login = null;
-    EditText pass = null;
-    Button loginBtn = null;
+    TextView textView = null;
+    Button textBtn = null;
 
     final String page_url = "https://wu.up.krakow.pl/WU/";
 
@@ -45,8 +50,6 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected String doInBackground(String... strings) {
-
-
             MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded");
             RequestBody body = RequestBody.create(mediaType, strings[1]);
             Request request = new Request.Builder()
@@ -59,9 +62,7 @@ public class MainActivity extends AppCompatActivity {
             try (Response response = client.newCall(request).execute()) {
                 Log.e("tag", response.message().toString());
                 try {
-                    //System.out.println("\n To jest print: "+response.body().string()+"    : no i tak\n");
                     return response.body().string();
-
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -74,13 +75,14 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(String body) {
             Document doc = Jsoup.parse(body); //
-
+            String document = doc.toString();
+            //textView.setText("Response is: "+ body.substring(0,500));
             try {
-                Element blad = doc.getElementById("ctl00_ctl001_ContentPlaceHolder_MiddleContentPlaceHolder_lblMessage");
+                Element blad = doc.getElementById("ctl00_ctl00_ContentPlaceHolder_MiddleContentPlaceHolder_lblMessage");
                 if (blad != null) {
-                    Toast.makeText(MainActivity.this, blad.text(), Toast.LENGTH_SHORT)
+                    Toast.makeText(OcenyActivity.this, blad.text(), Toast.LENGTH_SHORT)
                             .show();
-                    Log.e("tag", blad.text()); //tu byl komentarz
+//                    Log.e("tag", blad.text());
                     return;
                 }
             } catch (Exception e) {
@@ -93,9 +95,35 @@ public class MainActivity extends AppCompatActivity {
                     Log.e("--td -- ", td.text());
                 }
             }
-            Intent intent = new Intent(MainActivity.this, MenuActivity.class);
-            finish();
-             startActivity(intent);
+
+            //Pattern compiledPattern = Pattern.compile("tbody");
+            //Matcher matcher = compiledPattern.matcher(body);
+            //textView.setText(matcher.toString());
+            //System.out.println("\n To jest print body: "+matcher.toString()+"    : no i tak\n");
+            //String[] wynik = body.split("<tbody><tr class=\"gridDaneHead\" valign=\"bottom\">");
+            //for(String w : wynik){
+            //System.out.println("\n To jest print body: "+w+"    : no i tak\n");
+            //textView.setText(w);}
+            System.out.println("BODY: "+document);
+            String szukacz1 = "<table class=\"gridPadding fill\" cellspacing=\"0\" cellpadding=\"0\" rules=\"all\" border=\"1\" id=\"ctl00_ctl00_ContentPlaceHolder_RightContentPlaceHolder_dgDane\">";
+            String szukacz2 = "</table>";
+            int p1, p2;
+            for (int i = 0; i < doc.toString().length(); i++) {
+                p1 = document.indexOf(szukacz1);
+                p2 = document.indexOf(szukacz2);
+                System.out.println("\n\n\n\n"+p1+" "+p2+"\n\n\n\n");
+                if (p1 > 0 && p2 > 0) {
+                    System.out.println("Szukane frazy wystepują na pozycji: ");
+                    System.out.println(+(p1 + 1) + " oraz: " + (p2 + 1));
+                    System.out.println("\n" + document.substring((p1), (p2)));
+                    textView.setText(document.substring((p1), (p2)));
+                    break;
+                } else {
+                    System.out.println("Szukana fraza nie wystepuje");
+                    break;
+                }
+            }
+
         }
 
     }
@@ -104,25 +132,27 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_oceny);
 
-        login = findViewById(R.id.login);
-        pass = findViewById(R.id.password);
-        loginBtn = findViewById(R.id.login_button);
-        final ProgressBar simpleProgressBar = (ProgressBar) findViewById(R.id.progressBar);
 
-        loginBtn.setOnClickListener(v -> {
-            String slogin = login.getText().toString();
-            String spass = pass.getText().toString();
-            simpleProgressBar.setVisibility(View.VISIBLE);
+        textBtn = findViewById(R.id.btnOceny);
+        textView = findViewById(R.id.tvOceny);
 
-            String bodyParams = "ctl00%24ctl00%24ContentPlaceHolder%24MiddleContentPlaceHolder%24txtIdent=" + slogin + "&ctl00%24ctl00%24ContentPlaceHolder%24MiddleContentPlaceHolder%24txtHaslo=" + spass
+
+        textBtn.setOnClickListener(v -> {
+
+
+
+            String bodyParams = "ctl00%24ctl00%24ContentPlaceHolder%24MiddleContentPlaceHolder%24txtIdent=" + "login" + "&ctl00%24ctl00%24ContentPlaceHolder%24MiddleContentPlaceHolder%24txtHaslo=" + "password"
                     + "&ctl00%24ctl00%24ContentPlaceHolder%24MiddleContentPlaceHolder%24butLoguj=Zaloguj"
                     + "&__VIEWSTATE=%2FwEPDwUKMTgxMTA3NTE0Mw8WAh4DaGFzZRYCZg9kFgJmD2QWAgIBD2QWBAICD2QWAgIBD2QWAgIBD2QWAgICDxQrAAIUKwACDxYEHgtfIURhdGFCb3VuZGceF0VuYWJsZUFqYXhTa2luUmVuZGVyaW5naGRkZGQCBA9kFgICAw9kFg4CAQ8WAh4JaW5uZXJodG1sBS1XaXJ0dWFsbmEgVWN6ZWxuaWE8IS0tIHN0YXR1czogNzcyMjA2MTI1IC0tPiBkAg0PDxYCHgRNb2RlCyolU3lzdGVtLldlYi5VSS5XZWJDb250cm9scy5UZXh0Qm94TW9kZQJkZAIVDw8WAh4EVGV4dAUZT2R6eXNraXdhbmllIGhhc8WCYTxiciAvPmRkAhcPDxYCHgdWaXNpYmxlaGQWAgIDDxBkDxYCZgIBFgIFB3N0dWRlbnQFCGR5ZGFrdHlrFgFmZAIZD2QWBAIBDw8WAh8FBTQ8YnIgLz5MdWIgemFsb2d1aiBzacSZIGpha28gc3R1ZGVudCBwcnpleiBPZmZpY2UzNjU6ZGQCAw8PFgIfBQUIUHJ6ZWpkxbpkZAIbDw8WBB8FBRhTZXJ3aXMgQWJzb2x3ZW50w7N3PGJyLz4fBmhkZAIfDw8WAh8GaGRkGAEFHl9fQ29udHJvbHNSZXF1aXJlUG9zdEJhY2tLZXlfXxYBBUpjdGwwMCRjdGwwMCRUb3BNZW51UGxhY2VIb2xkZXIkVG9wTWVudUNvbnRlbnRQbGFjZUhvbGRlciRNZW51VG9wMyRtZW51VG9wM71u5cvxo3%2F6OarM3JXDhn%2F9bImN&__VIEWSTATEGENERATOR=7D6A02AE";
             NetworkTask task = new NetworkTask();
             task.execute(page_url + "Logowanie2.aspx?returnUrl=/WU/OcenyP.aspx", bodyParams);
 
 
+
+
         });
     }
 }
+
